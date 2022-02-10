101caf83170870553768ab45090f694669a951bb
Suhan Paradkar <12suhangp34@gmail.com>
2022-02-10 08:30:53 +0530

    Remove awaisomereport/

--- app/src/main/java/awaisomereport/ICrashHandler.kt
+++
@@ -1,9 +0,0 @@
-package awaisomereport
-
-interface ICrashHandler {
-    fun uncaughtException(
-        t: Thread,
-        exception: Throwable,
-        defaultExceptionHandler: Thread.UncaughtExceptionHandler
-    )
-}
